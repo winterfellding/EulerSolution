3aa5b72b07d1157daf00276cea5a0ec51c5a678f
winterfellding <winterfall.ding@gmail.com>
2013-12-11 17:22:50 +0800

    Problem 3

---
+++ q3.py
@@ -0,0 +1,17 @@
+import math
+
+def biggestPrimerFactor(num):
+  i = 2
+  factor = 1
+  sqrtNum = math.sqrt(num)
+  while i < sqrtNum:
+    if num % i == 0:
+      num = num / i
+      factor = i
+    else:
+      i += 1
+  return factor
+
+if __name__ == '__main__':
+  print biggestPrimerFactor(13195)
+  print biggestPrimerFactor(600851475143)
