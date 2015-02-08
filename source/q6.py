05ee9d877216ee2b975875d1a4440a596be987db
winterfellding <winterfall.ding@gmail.com>
2015-02-08 23:29:07 +0800

    refactor to method

--- source/q6.py
+++ source/q6.py
@@ -1,8 +1,10 @@
-sumOfSquare = 0
-squareOfSum = 0
-sum1 = 0
-for i in range(1, 101, 1):
-    sumOfSquare += i ** 2
-    sum1 += i
-squareOfSum = sum1 ** 2
-print squareOfSum - sumOfSquare
+def square_of_sum_minus_sum_of_square(number):
+    sum_of_square = 0
+    square_of_sum = 0
+    sum1 = 0
+    for i in range(1, number + 1, 1):
+        sum_of_square += i ** 2
+        sum1 += i
+    square_of_sum = sum1 ** 2
+    return square_of_sum - sum_of_square
+print square_of_sum_minus_sum_of_square(100)
